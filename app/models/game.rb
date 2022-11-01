48d485a9e723dbc1f021c70a8b585ad583a71172
atte <atukka@gmail.com>
2022-11-01 14:33:16 +0200

    refactor grid iteration

--- app/models/game.rb
+++ app/models/game.rb
@@ -7,5 +7,11 @@ class Game < ApplicationRecord
   HEX_DATA = {}
   COL_ALPHA = (('A'.ord)..('A'.ord+MAP_COLS)).map(&:chr)
 
-  
+  def grid_enumerator
+    (HEX_W..HEX_W*MAP_COLS).step(HEX_W).each_with_index do |x, i|
+      (HEX_H..HEX_H*MAP_ROWS).step(HEX_H).each_with_index do |y, j|
+        yield x,y,i,j
+      end
+    end
+  end
 end
