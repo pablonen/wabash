388b4007387a66bc383bcc1ede951c2ebb353c3c
atte <atukka@gmail.com>
2022-11-01 18:26:35 +0200

    rails importmap:install & rails stimulus:install

---
+++ app/javascript/controllers/hex_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="hex"
+export default class extends Controller {
+  connect() {
+    console.log("asdf")
+  }
+
+  
+}
